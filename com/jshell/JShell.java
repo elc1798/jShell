9c01a9a17ffee35a1cbc933f7ad75882c26f19aa
elc1798 <subliminalmau5@gmail.com>
2015-11-13 11:17:08 -0500

    Began working on prompt support, will probably leave for later. Low priority

--- com/jshell/JShell.java
+++ com/jshell/JShell.java
@@ -1,10 +1,31 @@
 package com.jshell;
 
-import java.io.File;
-
 import static com.jshell.CONSTANTS.JSH_TEMP;
 
+import java.io.File;
+
+import com.jshell.filesystem.DirectoryTree;
+
 public class JShell {
+
+    private static String prompt = System.getProperty("user.name") + ":%s $ ";
+    private static String[] known_fs_commands = {
+        "cd", "ls", "cp", "mv", "touch"
+    };
+
+    /*
+     * TODO:
+     private void loadConfigFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader(JSHRC))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+
+            }
+        } catch(Exception e) {
+
+        }
+     }
+     */
 
     public static void main(String[] args) {
         // Setup JShell temporary directory for backup files
@@ -15,5 +36,11 @@ public class JShell {
         }
         JSHELL_TEMP_DIRECTORY.mkdir();
         JSHELL_TEMP_DIRECTORY = null;
+
+        DirectoryTree fs = new DirectoryTree();
+        while (true) {
+            System.out.printf(prompt, fs.getWorkingDirectory());
+
+        }
     }
 }
---
+++ com/jshell/JShellPrompt.java
@@ -0,0 +1,4 @@
+package com.jshell;
+
+public class JShellPrompt {
+}
