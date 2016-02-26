3f551cfe2eeb5ea9b199d2f884a20c39edda376e
elc1798 <subliminalmau5@gmail.com>
2016-02-26 14:31:03 -0500

    Make constants file an interface, rather than a class

--- org/jshell/Constants.java
+++ org/jshell/Constants.java
@@ -1,5 +1,13 @@
 package org.jshell;
 
-public class Constants {
-
+/**
+ * Interface containing project constant values for easy accessibility and
+ * refactoring. This is an interface rather than a class so all the values are
+ * inherently public, which saves typing.
+ *
+ * @author elc1798
+ *
+ */
+public interface Constants {
+    String cwdSystemProperty = "user.dir";
 }
