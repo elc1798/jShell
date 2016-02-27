9c42c573221ce1c062348ae49c8d10452ddcf6a4
elc1798 <subliminalmau5@gmail.com>
2016-02-27 17:06:16 -0500

    Add Javadoc in EnvVarTracker

--- org/jshell/JshellEnvVarTracker.java
+++ org/jshell/JshellEnvVarTracker.java
@@ -2,22 +2,49 @@ package org.jshell;
 
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * A HashMap to keep track of Jshell environment variables, aliases, functions,
+ * etc. At its core, {@code JshellEnvVarTracker} is a {@link ConcurrentHashMap}.
+ *
+ * @author photoXin
+ *
+ */
 public class JshellEnvVarTracker {
 
     private ConcurrentHashMap<String, JshellObject> dictionary;
 
+    /**
+     * Creates a new {@code JshellEnvVarTracker} with key type {@link String}
+     * and value type {@link JshellObject}.
+     */
     public JshellEnvVarTracker() {
         dictionary = new ConcurrentHashMap<String, JshellObject>();
     }
 
+    /**
+     * @param key - A String that represents a key for the HashMap
+     * @return True if the key exists, false otherwise
+     */
     public boolean contains(String key) {
         return dictionary.containsKey(key);
     }
 
+    /**
+     * @param key - A String that represents a key for the HashMap
+     * @return The value that is represented by the provided key, {@code null}
+     *         otherwise
+     */
     public JshellObject get(String key) {
         return dictionary.get(key);
     }
 
+    /**
+     * Inserts a new JshellObject ({@code jobj}) into the HashMap, with the
+     * provided key.
+     *
+     * @param key - A String that represents a key for the HashMap
+     * @param jobj - A JshellObject
+     */
     public void set(String key, JshellObject jobj) {
         dictionary.put(key, jobj);
     }
