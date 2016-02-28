1b1c0659e8a0f0de8efb87977ff877bf6b6430c3
elc1798 <subliminalmau5@gmail.com>
2016-02-28 17:47:30 -0500

    Add prompt command that reads from System.in and runs the appropriate command

--- org/jshell/commands/JshellLeftPrompt.java
+++ org/jshell/commands/JshellLeftPrompt.java
@@ -1,41 +1,48 @@
 package org.jshell.commands;
 
+import java.util.Scanner;
+
+import org.jshell.Jshell;
 import org.jshell.utils.JshellCommand;
 
 public class JshellLeftPrompt extends JshellCommand {
 
+    private String prompt;
+    private String input;
+    private Scanner userIn;
+
     public JshellLeftPrompt() {
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public void initialize() throws Exception {
-        // TODO Auto-generated method stub
-
+        // Get the prompt. This will later be put into an env variable
+        prompt = "$ ";
+        // Print the prompt
+        System.out.print(prompt);
+        // Create the Scanner
+        userIn = new Scanner(System.in);
     }
 
     @Override
     public void execute() throws Exception {
-        // TODO Auto-generated method stub
-
+        input = userIn.nextLine();
     }
 
     @Override
     public boolean isFinished() throws Exception {
-        // TODO Auto-generated method stub
-        return false;
+        return input != null;
     }
 
     @Override
     public void end() throws Exception {
-        // TODO Auto-generated method stub
-
+        Jshell.scheduler.schedule(new JshellProcRunner(input.split(" ")));
+        userIn.close();
     }
 
     @Override
     public void interrupted() {
-        // TODO Auto-generated method stub
-
+        userIn.close();
     }
 
 }
